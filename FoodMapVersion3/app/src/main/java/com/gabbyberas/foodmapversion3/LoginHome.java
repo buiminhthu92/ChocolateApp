beb3eb757739d013c160b1e661ad566008afd0f7
GabbyBears <progitteam@googlegroups.com>
2015-10-15 13:19:11 +0700

    Update lan 2  ngay 15/10/2015

---
+++ FoodMapVersion3/app/src/main/java/com/gabbyberas/foodmapversion3/LoginHome.java
@@ -0,0 +1,7 @@
+package com.gabbyberas.foodmapversion3;
+
+/**
+ * Created by Android on 10/15/2015.
+ */
+public class LoginHome {
+}
---
+++ MyApplication/app/src/main/java/com/gabbybears/showimage/MainActivity.java
@@ -0,0 +1,10 @@
+package com.gabbybears.showimage;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+
+
+}
